4b029327abbcef3cd2b5e1e109d4f864308c7eba
MohamedEl-Masry <mohamed.elmasry9722@gmail.com>
2023-11-25 07:07:26 +0200

    Second Commit

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./src/App.jsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
